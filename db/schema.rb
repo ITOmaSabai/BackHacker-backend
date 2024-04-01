4ba055dbb5370c3d62dbbb08dcb8ad88e62c8030
ItoSbai <lualaa.design2565@gmail.com>
2024-04-01 19:36:14 +0900

    add:Addressモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_082749) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_103412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "country"
+    t.string "administrative_area_level_1"
+    t.string "formatted_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "spots", force: :cascade do |t|
     t.string "name"
