be103c60fbb44aeffb9339930640ae945335731b
ItoSbai <lualaa.design2565@gmail.com>
2024-04-01 19:40:20 +0900

    add:likeモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_103936) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_104001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_01_103936) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
