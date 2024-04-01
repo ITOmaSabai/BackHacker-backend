affb9ca8ad2dfc793ce8c59996cf9519102fc32a
ItoSbai <lualaa.design2565@gmail.com>
2024-04-01 19:28:03 +0900

    add:Spotモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_081049) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_082749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spots", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "lat"
+    t.float "lng"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
